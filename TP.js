e7b1b6b9a23b0302cd762de3f26d7fe45f523b96
lyeslimani <71546957+lyeslimani@users.noreply.github.com>
2022-01-19 01:59:30 +0100

    first coimmit

---
+++ TP.js
@@ -0,0 +1,13 @@
+import * as THREE from 'three'
+
+const scene = new THREE.Scene();
+
+// Configuration cameras
+let camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 1, 1000 );
+camera.position.set( 0, 10, 0 );
+
+// Configuration du renderer
+let renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
